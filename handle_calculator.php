b9c607fe0cd10efaacf008dc0bd88cd53fdc84a0
nurnabilahhh <nabilahshaari01@gmail.com>
2022-04-27 18:10:11 +0800

    5th commit

--- handle_calculator.php
+++ handle_calculator.php
@@ -12,9 +12,18 @@
         if($source == 'calculator.html'){
             if ((is numeric($quantity))){
                 $total = ($quantity * $price) * ($taxrate +1);
-                $
+                $total = number_format ($total,2);
+                echo "You are purchasing <b>".$_POST["quantity"]. 
+                "</b> widgets at a cost of <b>". $_POST["price"]. 
+                "</b> each. With tax the total comes to <b>". $total . "</b>.\n";
+            }else{
+                echo "<p><b>Please enter a valid quantity to purchase!</b></p>";
             }
+        }else{
+            echo "<p><b>You have accessed this page inappropriately!</b></p>";
         }
+    }else{
+        echo "<p><b>You have accessed this page inappropriately!</b></p>";
     }
 
     ?>
